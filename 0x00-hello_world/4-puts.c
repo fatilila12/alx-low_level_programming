c7fea884a3f2c5a24dd06b1de27351dfe8507e31
fatilila12 <mahert268@gmail.com>
2023-09-07 13:47:27 -0700

    prints exactly "Programming is like building a multilingual puzzle,

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/*
+ * main is a entry point of a C program
+ */
 
-int main() 
+int main(void) 
 {
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
